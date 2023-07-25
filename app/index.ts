5e45dd6538065a541c97e3ea243ac1922af9de6f
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2023-07-25 11:04:57 +0300

    feat: Server was created, and route definitions were made

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,31 @@
-console.log("deneme");
+import express, {Request,Response} from "express"
+
+//uygulama oluştur
+const app = express();
+
+//router oluştur
+const router = express.Router();
+
+//fake data interfacei
+interface User{
+    id: string
+    ad: string
+    soyad: string
+}
+
+//route
+router.get("/user", (req:Request, res:Response) => {
+    //fake datayı doldur ve response olarak gönder
+    const arr:User[] = [{ad:"Ali", id:"1", soyad:"Uçar"}, {ad:"Ayşe", id:"2", soyad:"Uçar"}];
+    res.status(200).json(arr); 
+});
+router.post("/user", (req:Request, res:Response) => {
+    const obj:User = {ad:"Yasin", soyad:"Dalkılıç", id:"5"};
+    res.status(201).json(obj);
+});
+
+app.use("/", router);
+//serveri dinle
+app.listen(3001, () => {
+    console.log("3001");
+});
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("deneme");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+//uygulama oluştur
+var app = (0, express_1.default)();
+//router oluştur
+var router = express_1.default.Router();
+//route
+router.get("/user", function (req, res) {
+    //fake datayı doldur ve response olarak gönder
+    var arr = [{ ad: "Ali", id: "1", soyad: "Uçar" }, { ad: "Ayşe", id: "2", soyad: "Uçar" }];
+    res.status(200).json("arr");
+});
+//serveri dinle
+app.listen(3001, function () {
+    console.log("3001");
+});
