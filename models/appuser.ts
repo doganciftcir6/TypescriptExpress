001e92c7dc41bc8d0b5d8a2376a280610d5a12b1
Dogan <114991786+doganciftcir6@users.noreply.github.com>
2023-07-26 12:29:17 +0300

    feat: The Product model was created

---
+++ models/appuser.ts
@@ -0,0 +1,46 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../helpers/dbhelpers/connection";
+import bcrypt from "bcrypt";
+import IAppUser from "./interfaces/userinterface";
+
+class AppUser extends Model<IAppUser> implements IAppUser{
+    public id!: number;
+    public username!: string;
+    email!: string;
+    password!: string;
+}
+
+AppUser.init(
+    {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: "AppUser",
+    }
+);
+
+//Kullanıcının create olma durumunda şifresini dbye hashleyerek kayıt et
+//db işlemi olduğu için uzun süren bir işlem dolayısıyla asekron olmalı
+AppUser.beforeCreate(async  (user: AppUser) => {
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+});
+
+export default AppUser
---
+++ models/interfaces/userinterface.ts
@@ -0,0 +1,8 @@
+interface IAppUser {
+  id?: number;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export default IAppUser
